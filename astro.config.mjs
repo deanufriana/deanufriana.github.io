a01ad749c50dc3839905819921ab21d89af5cee8
deanufriana <deanheart09@gmail.com>
2025-10-27 19:07:31 +0700

    feat: add resume page and integrate PDF generation
    - Introduced a new resume page using Astro, displaying personal and professional details.
    - Implemented PDF generation for the resume using the astro-pdf integration.
    - Added a Prettier configuration file for consistent code formatting.
    - Updated package dependencies to include @fontsource/lato for font styling.
    - Refactored date formatting logic into a separate utility function for better code organization.
    - Created global and resume-specific CSS styles for improved layout and print formatting.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,16 +8,47 @@ import vue from "@astrojs/vue";
 
 import tailwindcss from '@tailwindcss/vite';
 
+import pdf from 'astro-pdf';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://deanufriana.github.io/',
 
-  integrations: [mdx(), sitemap(), vue(),
-  partytown({
+  integrations: [mdx(), sitemap(), vue(), partytown({
     // Adds dataLayer.push as a forwarding-event.
     config: {
       forward: ["dataLayer.push"],
     },
+  }),
+  pdf({
+    pages: {
+      '/resume': {
+        path: 'resume.pdf',
+        pdf: { // Puppeteer PDFOptions
+          format: 'A4',
+          printBackground: true,
+          displayHeaderFooter: true,
+          headerTemplate: `
+            <div style="font-size: 10px; width: 100%; text-align: right; padding-right: 20px;">
+              <span class="title"></span>
+            </div>
+          `,
+          footerTemplate: `
+            <div style="font-size: 10px; width: 100%; padding-left: 20px; padding-right: 20px; display: flex; justify-content: space-between;">
+              <span>Devi Adi Nufriana - Software Engineer</span>
+              <span class="pageNumber"></span>/<span class="totalPages"></span>
+            </div>
+          `,
+          margin: {
+            top: '60px',
+            right: '40px',
+            bottom: '60px',
+            left: '40px'
+          },
+          timeout: 20_000
+        },
+      }
+    }
   })],
 
   vite: {
